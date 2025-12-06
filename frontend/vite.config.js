0f1f6a696c0e4c44e20cc12ae6ed346296f90357
tithi <tithishah26@gmail.com>
2025-12-06 17:55:10 +0530

    Add proxy configuration to Vite server for API requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      "/api": "http://localhost:5000"
+    }
   }
 });
 

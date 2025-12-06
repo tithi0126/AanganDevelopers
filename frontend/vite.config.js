5a9cd37c4afb934df94cacaac54808d39cc23008
Dev Jariwala <jariwaladev14@gmail.com>
2025-12-07 00:26:20 +0530

    url chnaged for api call in proxy and everything live on server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:5000"
+      "/api": "https://aangandevelopers.com/"
     }
   }
 });
